244cea1f680cd56452124c069640e6f0f8d97e3d
alok kumar maurya <alokkumarmaurya5556@gmail.com>
2024-07-28 01:05:03 +0530

    working on DB

--- chat_application/packages/modules/chat/lib/src/presentation/controller/chat_list/chat_list_controller.dart
+++ chat_application/packages/modules/chat/lib/src/presentation/controller/chat_list/chat_list_controller.dart
@@ -2,26 +2,27 @@ import 'package:chat_module/src/domain/usecase/chat_useCase/chat_useCase.dart';
 import 'package:network_service/network_service.dart';
 import 'package:auth/auth.dart';
 import '../../../data/models/response/get_all_chat_list_response.dart';
+
 class ChatListController extends GetxController {
   bool isLoading = false;
   List<GetAllChatListResponse> chatList = [];
-String userPhone="";
+  String userPhone = "";
   final ChatUseCase _chatUseCase = Get.find();
 
   @override
   void onInit() {
     FirebaseAuth auth = FirebaseAuth.instance;
-    userPhone=auth.currentUser!.phoneNumber??"";
+    userPhone = auth.currentUser!.phoneNumber ?? "";
     getChatList();
     super.onInit();
   }
 
   Future<void> getChatList() async {
-  return await _chatUseCase.getAllChatList(mobileNumber: userPhone).then(
+    return await _chatUseCase.getAllChatList(mobileNumber: userPhone).then(
       (value) {
         chatList = value.chatList;
         update();
-        return ;
+        return;
       },
     );
   }
